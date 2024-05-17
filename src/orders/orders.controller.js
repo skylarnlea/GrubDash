311e7591f76b25ce6b96149189a546c93a2602df
Skylar Lea <skyniclea@gmail.com>
2024-05-17 11:27:51 -0500

    break

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -7,3 +7,100 @@ const orders = require(path.resolve("src/data/orders-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /orders handlers needed to make the tests pass
+
+
+/////////////Validation Middleware below/////////////
+//deliverTo property is missing
+function hasDeliverTo(req, res, next){
+    const { data: { deliverTo } = {} } = req.body;
+    if (deliverTo){
+        return next();
+    }
+    next({
+        status: 400,
+        message: "Order must include a deliverTo"
+    });
+}
+
+//delverTo property is empty
+function deliverToHasText(req, res, next){
+    const { data: { deliverTo } = {} } = req.body;
+    if (deliverTo === ""){
+        next({
+            status: 400,
+            message: "Order must include a deliverTo",
+        })
+    }
+    next();
+}
+
+//mobileNumber property is missing
+function hasMobileNumber(req, res, next){
+    const { data: { mobileNumber } = {} } = req.body;
+    if (mobileNumber){
+        return next();
+    }
+    next({
+        status: 400,
+        message: "Order must include a mobileNumber",
+    });
+}
+
+//mobileNumber property is empty
+function mobileNumberHasText(req, res, next){
+    const { data: { mobileNumber } = {} } = req.body;
+    if (mobileNumber === ""){
+        next({
+            status: 400,
+            message: "Order must include a mobileNumber",
+        })
+    }
+    next();
+}
+
+//dishes property is missing 
+function hasDishes(req, res, next){
+    const { data: { dishes } = {} } = req.body;
+    if (dishes){
+        return next();
+    }
+    next({
+        status: 400,
+        message: "Order must include a dish"
+    });
+}
+
+//dishes property is not an array
+function dishesArray(req, res, next){
+    const { data: { dishes } = {} } = req.body;
+    if (Array.isArray(dishes)){
+        return next();
+    }
+    next({
+        status: 400, 
+        message: "Order must include at least one dish"
+    });
+}
+
+//dishes array is empty
+function dishesEmptyArray(req, res, next){
+    const { data: { dishes } = {} } = req.body;
+    if (dishes.length === 0){
+        return next({
+            status: 400,
+            message: "Order must include at least one dish"
+        });
+    };
+}
+
+//dish quantity property missing
+function hasQuantity(req, res, next){
+    const { data: { quantity } = {} } = req.body;
+    if (quantity){
+        return next();
+    }
+    next({
+        status: 400,
+        message: `Dish ${index} must have a quantity that is an integer greater than 0`,
+    });
+}
--- src/orders/orders.router.js
+++ src/orders/orders.router.js
@@ -1,5 +1,8 @@
 const router = require("express").Router();
+const controller = require("./orders.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
 
 // TODO: Implement the /orders routes needed to make the tests pass
+
 
 module.exports = router;
