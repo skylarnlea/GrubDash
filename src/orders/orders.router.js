7cb69bb435d54c3b7c099feb56a4f82ba032f29c
Skylar Lea <skyniclea@gmail.com>
2024-05-17 14:53:17 -0500

    orders router first commit

--- src/orders/orders.router.js
+++ src/orders/orders.router.js
@@ -4,5 +4,18 @@ const methodNotAllowed = require("../errors/methodNotAllowed");
 
 // TODO: Implement the /orders routes needed to make the tests pass
 
+router
+    .route('/:orderId')
+    .get(controller.read)
+    .put(controller.update)
+    .delete(controller.delete)
+    .all(methodNotAllowed);
 
+router
+    .route('/')
+    .get(controller.list)
+    .post(controller.create)
+    .all(methodNotAllowed);
+
+    
 module.exports = router;
